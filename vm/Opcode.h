0bfb98139a4e573ff29656941c134acd5fb3d2a2
Robert Nelson <nelsorober@gmail.com>
2012-01-21 01:04:08 -0500

    Add constant type to opcode.h

--- vm/Opcode.h
+++ vm/Opcode.h
@@ -3,7 +3,7 @@
 
 #define ADDR_SIZE sizeof(unsigned int)
 
-#define load_argn(arg, size) memcpy((char*)&arg, &mText[eip], size); eip += size;
+#define load_argn(arg, size) memcpy((char*)&arg, &mText[EIP], size); EIP += size;
 #define load_arg(arg) load_argn(arg, ADDR_SIZE)
 
 enum ArgTypes {
@@ -11,6 +11,7 @@ enum ArgTypes {
 	TYPE_Register,
 	TYPE_Address,
 	TYPE_Id,
+	TYPE_Constant,
 	NUM_TYPES
 };
 
