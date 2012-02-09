f13846ae0f28e4d3b4e67eac559ade64cb73ce11
Robert Nelson <nelsorober@gmail.com>
2012-02-09 00:15:27 -0500

    Fixed string constant handling in assembler

--- assembler/Instruction.cpp
+++ assembler/Instruction.cpp
@@ -91,6 +91,11 @@ void Instruction::ParseArguments()
 			token = mLine.substr(tokenBegin, tokenEnd - tokenBegin);
 		}
 
+		if(token[0] == '"') {
+			tokenEnd = mLine.find_first_of("\"", tokenBegin + 1) + 1;
+			token = mLine.substr(tokenBegin, tokenEnd - tokenBegin);
+		}
+
 		mArguments.push_back(new Argument(token));
 	}
 }
