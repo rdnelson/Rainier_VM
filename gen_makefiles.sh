be31edde4cf3450043d34a527d745e8aff5349c0
Robert Nelson <nelsorober@gmail.com>
2012-02-13 00:35:18 -0500

    gen_makefiles now handles projects with no source code better

--- gen_makefiles.sh
+++ gen_makefiles.sh
@@ -4,15 +4,19 @@ PROJECTS=$( find . \( -type d -a ! -path "./.*" -a ! -path "./*/*" -a ! -path ".
 for DIR in $PROJECTS
 do
 	cd $DIR
+	NUMFILES=$( find . -maxdepth 1 \( -type "f" -a ! -path "." -a -path "./*.cpp" -a ! -path "./.*" \) | wc -l )
 	echo "include ../make.common" > makefile
 	echo "OBJS=" >> makefile
 	echo .PHONY: all >> makefile
 	echo "include \$(shell find . -iname \"*cpp.d\" -printf \"%h/%f \" )" >> makefile
 	#echo "\$(warning OBJS: \$(OBJS))" >> makefile
-	echo $( echo $DIR | tail -c +3 ): "\$(OBJS)" >> makefile 
-	echo "	\$(CC) \$(DEBUG) \$(LFLAGS) -o \$@ \$^" >> makefile
-	echo "	\$(MKDIR) \$(BINDIR)" >> makefile
-	echo "	\$(CP) \$@ \$(BINDIR)/\$@" >> makefile
+	echo $( echo $DIR | tail -c +3 ): "\$(OBJS)" >> makefile
+	if [ $NUMFILES != 0 ]
+	then
+		echo "	\$(CC) \$(DEBUG) \$(LFLAGS) -o \$@ \$^" >> makefile
+		echo "	\$(MKDIR) \$(BINDIR)" >> makefile
+		echo "	\$(CP) \$@ \$(BINDIR)/\$@" >> makefile
+	fi
 	echo >> makefile
 	echo "all: $( echo $DIR | tail -c +3 )" >> makefile
 	cd ..
