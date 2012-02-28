3376f8cfcf70029146d96da53fc09790d5a643a4
Robert Nelson <nelsorober@gmail.com>
2012-02-28 12:31:10 -0500

    Implemented bitshift operators

--- vm/Shl.cpp
+++ vm/Shl.cpp
@@ -1,6 +1,8 @@
 #include "Shl.h"
 #include "VM.h"
 
+#include "common/Opcode.h"
+
 Shl::Shl(char* eip)
 {
 	mEipOffset += LoadArgs(2, eip);
@@ -8,6 +10,24 @@ Shl::Shl(char* eip)
 
 void Shl::Execute()
 {
-	VM_INSTANCE()->GetMemSize();
+	ResolveValue(1);
+	unsigned int val1;
+	unsigned int shl;
+
+	switch(subcode[0]) {
+	case SC_REG:
+		val1 = VM_INSTANCE()->GetRegister(arguments[0]);
+		shl = val1 << arguments[1];
+		VM_INSTANCE()->SetRegister(arguments[0], shl);
+
+		if(shl == 0)
+			VM_INSTANCE()->SetFlag(FLAG_ZERO);
+		else
+			VM_INSTANCE()->ClearFlag(FLAG_ZERO);
+		break;
+
+	default:
+		VM_INSTANCE()->GetLogger() << "Invalid First Operand For Shr: 0x" << std::hex << subcode[0] << std::dec << std::endl;
+	}
 }
 
--- vm/Shr.cpp
+++ vm/Shr.cpp
@@ -1,6 +1,8 @@
 #include "Shr.h"
 #include "VM.h"
 
+#include "common/Opcode.h"
+
 Shr::Shr(char* eip)
 {
 	mEipOffset += LoadArgs(2, eip);
@@ -8,6 +10,24 @@ Shr::Shr(char* eip)
 
 void Shr::Execute()
 {
-	VM_INSTANCE()->GetMemSize();
+	ResolveValue(1);
+	unsigned int val1;
+	unsigned int shr;
+
+	switch(subcode[0]) {
+	case SC_REG:
+		val1 = VM_INSTANCE()->GetRegister(arguments[0]);
+		shr = val1 >> arguments[1];
+		VM_INSTANCE()->SetRegister(arguments[0], shr);
+
+		if(shr == 0)
+			VM_INSTANCE()->SetFlag(FLAG_ZERO);
+		else
+			VM_INSTANCE()->ClearFlag(FLAG_ZERO);
+		break;
+
+	default:
+		VM_INSTANCE()->GetLogger() << "Invalid First Operand For Shr: 0x" << std::hex << subcode[0] << std::dec << std::endl;
+	}
 }
 
