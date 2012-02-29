d6eb500d81340f8c0fc1d468cb7f9b08f8f93d8a
Robert Nelson <nelsorober@gmail.com>
2012-02-28 21:04:42 -0500

    Push and pop implemented

--- vm/Pop.cpp
+++ vm/Pop.cpp
@@ -1,6 +1,9 @@
 #include "Pop.h"
 #include "VM.h"
 
+#include "common/Opcode.h"
+#include <cstring>
+
 Pop::Pop(char* eip)
 {
 	mEipOffset += LoadArgs(1, eip);
@@ -8,6 +11,18 @@ Pop::Pop(char* eip)
 
 void Pop::Execute()
 {
-	VM_INSTANCE()->GetMemSize();
+	unsigned int tmp;
+	int esp;
+	switch(subcode[0])
+	{
+	case SC_REG:
+		esp = VM_INSTANCE()->GetRegister(REG_ESP);
+		memcpy(&tmp, &VM_INSTANCE()->Memory[esp], 4);
+		VM_INSTANCE()->SetRegister(arguments[0], tmp);
+		break;
+	default:
+		VM_INSTANCE()->GetLogger() << "Cannot pop to non-register value" << std::endl;
+	}
+
 }
 
--- vm/Push.cpp
+++ vm/Push.cpp
@@ -1,6 +1,8 @@
 #include "Push.h"
 #include "VM.h"
 
+#include <cstring>
+
 Push::Push(char* eip)
 {
 	mEipOffset += LoadArgs(1, eip);
@@ -8,6 +10,11 @@ Push::Push(char* eip)
 
 void Push::Execute()
 {
-	VM_INSTANCE()->GetMemSize();
+	ResolveValue(0);
+
+	unsigned int esp = VM_INSTANCE()->GetRegister(REG_ESP);
+	memcpy(&VM_INSTANCE()->Memory[esp], &arguments[0], 4);
+	esp -= 4;
+	VM_INSTANCE()->SetRegister(REG_ESP, esp);
 }
 
