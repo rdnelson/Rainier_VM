5325f30468ad4e49da131624259764f945eeda94
Robert Nelson <nelsorober@gmail.com>
2012-02-29 13:20:47 -0500

    For rebase

--- vm/Add.cpp
+++ vm/Add.cpp
@@ -16,9 +16,22 @@ void Add::Execute()
 
 	switch(subcode[0]) {
 	case SC_REG:
-		//val1 = 
-		//sum = 
+		val1 = VM_INSTANCE()->GetRegister(argument[0]);
+		sum = val1 + argument[1];
+		VM_INSTANCE()->SetRegister(argument[0], sum);
+		if(sum < val1)
+			VM_INSTANCE()->SetFlag(CPU_OFL);
+		else
+			VM_INSTANCE()->ClearFlag(CPU_OFL);
+
+		if(sum == 0)
+			VM_INSTANCE()->SetFlag(CPU_ZERO);
+		else
+			VM_INSTANCE()->ClearFlag(CPU_ZERO);
 		break;
+
+	default:
+		VM_INSTANCE()->GetLogger() << "Invalid First Operand For Add: 0x" << std::hex << subcode[0] << std::dec << std::endl;
 	}
 }
 
