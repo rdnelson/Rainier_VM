78377c0170cc2cf2a8423e314117443e8f60c8a0
Robert Nelson <nelsorober@gmail.com>
2012-03-02 17:09:54 -0500

    Implemented Call and ret

--- vm/Call.cpp
+++ vm/Call.cpp
@@ -1,13 +1,24 @@
 #include "Call.h"
 #include "VM.h"
 
+#include <cstring>
+
 Call::Call(char* eip)
 {
-	mEipOffset += LoadArgs(2, eip);
+	mEipOffset += LoadArgs(1, eip);
 }
 
 void Call::Execute()
 {
-	VM_INSTANCE()->GetMemSize();
+	ResolveValue(0);
+	unsigned int esp = VM_INSTANCE()->GetRegister(REG_ESP);
+	unsigned int eip = VM_INSTANCE()->GetRegister(REG_EIP);
+	char* dst = VM_INSTANCE()->GetMemory(esp - 4);
+	if(dst) {
+		memcpy(dst, &eip, 4);
+		VM_INSTANCE()->SetRegister(REG_ESP, esp -4);
+		if(VM_INSTANCE()->GetMemory(arguments[0]))
+			VM_INSTANCE()->SetRegister(REG_EIP, arguments[0]);
+	}
 }
 
--- vm/Ret.cpp
+++ vm/Ret.cpp
@@ -1,6 +1,8 @@
 #include "Ret.h"
 #include "VM.h"
 
+#include <cstring>
+
 Ret::Ret(char* eip)
 {
 	mEipOffset += LoadArgs(2, eip);
@@ -8,6 +10,14 @@ Ret::Ret(char* eip)
 
 void Ret::Execute()
 {
-	VM_INSTANCE()->GetMemSize();
+	unsigned int esp = VM_INSTANCE()->GetRegister(REG_ESP);
+	unsigned int retAddr;
+	char* src = VM_INSTANCE()->GetMemory(esp);
+	if(src) {
+		memcpy(&retAddr, src, 4);
+		if(VM_INSTANCE()->GetMemory(retAddr))
+			VM_INSTANCE()->SetRegister(REG_EIP, retAddr);
+		VM_INSTANCE()->SetRegister(REG_ESP, esp + 4);
+	}
 }
 
